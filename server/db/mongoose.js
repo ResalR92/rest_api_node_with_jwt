e50e362dd1bda7ce0c7f3e184ec38ff34f1c42dd
Resal Ramdahadi <resalramdahadi92@gmail.com>
2017-12-11 11:44:04 +0700

    setting config ENV - DB - test/development/production

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,8 @@ mongoose.Promise = global.Promise;
 
 mongoose
   // .connect("mongodb://resal:resal@ds135156.mlab.com:35156/rest-api-nodejss", {
-  .connect("mongodb://localhost:27017/TodoApp", {
+  // .connect("mongodb://localhost:27017/TodoApp", {
+  .connect(process.env.MONGODB_URI, {
     useMongoClient: true
   })
   .then(() => console.log("MongoDB Connected"))
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,7 +10,7 @@ const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT; // no need to set default port
 // middleware bodyParser
 app.use(bodyParser.json());
 
